d8a6f9e17bbba3191e587219db23d3171872981e
husamahmud <husamahmud@gmail.com>
2024-05-03 02:18:04 +0300

    Add 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-"""task 1"""
+"""
+task 1
+"""
 
 
 def add(a: float, b: float) -> float:
-	"""takes two float args and return the float sum"""
+	"""
+	takes two float args and return the float sum
+	"""
 	return a + b
